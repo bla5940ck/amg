462386926202986eaddf64979786c0b78ea79862
1302572-NB\wistronits <bla5940ck@gmail.com>
2025-01-04 19:59:26 +0800

    disable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = '安聯AMG-法國巴黎';
   public amount: number = 0;
   public calculatedAmount: number | null = null;
@@ -13,21 +15,28 @@ export class AppComponent {
 
   public fristRate = 0.002 * 12;
   public secondRate = 0.0016 * 12;
-  public thridRate = 0.00134 * 12;
-  public fourthRate = 0.00134 * 12;
+  public thridRate = 0.001334 * 12;
+  public fourthRate = 0.001334 * 12;
 
   public tempAmout = 0;
 
   // 配權和配息的百分比
-  public percentAllocation: number | null = null;
-  public percentDividend: number | null = null;
+  public percentAllocation: number = 0;
+  public percentDividend: number  = 0;
   // 年化報酬率
   public irr: number = 0;
   // 保管費
   public storageFee: number = 1200;
 
+  public isAllWrite: boolean = true;
+
   // 初始化 years，這個陣列的每個元素是包含 year, allocation, dividend 的物件
   public years: { year: number, allocation: number, dividend: number }[] = [];
+
+  ngOnInit() {
+    // 初始化時檢查是否有必要的數值，並且更新按鈕狀態
+    this.checkAllValues();
+  }
 
   // 計算金額並將結果填充到每個年度
   calculateAmount() {
@@ -35,20 +44,18 @@ export class AppComponent {
 
     // 清空 years 陣列，以防止重複計算
     this.years = [];
-   
+
     // 配權金額與配息金額會依配權和配息百分比進行計算
     let allocationAmount = this.amount * (this.percentAllocation! / 100);
     let dividendAmount = this.amount * (this.percentDividend! / 100);
     for (let i = 1; i <= 4; i++) {
-  
-      console.log(allocationAmount)
       if(i == 2) {
         allocationAmount = this.calculateAllocationAmount(allocationAmount, this.fristRate);
       } else if(i == 3) {
         allocationAmount = this.calculateAllocationAmount(allocationAmount, this.secondRate);
       } else if(i == 4) {
         allocationAmount = this.calculateAllocationAmount(allocationAmount, this.thridRate);
-      } 
+      }
 
       // 每年都推送一個新的物件，包含年、配權金額和配息金額
       this.years.push({
@@ -63,7 +70,6 @@ export class AppComponent {
     return allocationAmount - this.storageFee - this.amount * yearRate + allocationAmount * this.irr * 0.01;
   }
 
-
   // 當其中一個百分比改變時，更新另一個
   onPercentChange(type: 'allocation' | 'dividend') {
     if (type === 'allocation') {
@@ -71,15 +77,28 @@ export class AppComponent {
     } else {
       this.percentAllocation = 100 - (this.percentDividend || 0); // 剩下的百分比給配權
     }
+
+    this.checkAllValues(); // 更新按鈕狀態
   }
 
   // 檢查百分比並確保它不超過100%
   checkPercentage(type: 'allocation' | 'dividend') {
-    if (type === 'allocation' && this.percentAllocation! > 100) {
-      this.percentAllocation = 100;
-    } else if (type === 'dividend' && this.percentDividend! > 100) {
-      this.percentDividend = 100;
+    if (type === 'allocation') {
+      // 讓 percentAllocation 的值不會小於 0 且不會大於 100
+      if (this.percentAllocation < 0) {
+        this.percentAllocation = 0; // 如果輸入負數，設為 0
+      } else if (this.percentAllocation > 100) {
+        this.percentAllocation = 100; // 如果超過 100，設為 100
+      }
+    } else if (type === 'dividend') {
+      // 讓 percentDividend 的值不會小於 0 且不會大於 100
+      if (this.percentDividend < 0) {
+        this.percentDividend = 0; // 如果輸入負數，設為 0
+      } else if (this.percentDividend > 100) {
+        this.percentDividend = 100; // 如果超過 100，設為 100
+      }
     }
+    this.checkAllValues();
   }
 
   // 計算配權和配息金額，並處理空值情況
@@ -91,4 +110,15 @@ export class AppComponent {
       return yearData.dividend;
     }
   }
+
+  // 檢查所有需要的值是否都不為0，來啟用/禁用按鈕
+  checkAllValues() {
+    this.isShow = false;
+    if (this.amount !== 0 && typeof(this.amount) == 'number' && (this.percentAllocation !== 0 || this.percentDividend !== 0) && this.irr !== 0 &&  typeof(this.irr) == 'number' && typeof(this.percentAllocation) == 'number') {
+      this.isAllWrite = false; // 所有必填項目都有填寫
+    } else {
+      this.isAllWrite = true; // 有未填寫的項目
+    }
+  }
+
 }
